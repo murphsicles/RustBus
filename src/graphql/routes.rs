819177b3cc22a7500ed3e23fe2b50a7a782c685f
Roy Murphy <roycoins@gmail.com>
2025-06-20 17:53:30 +0100

    Create routes.rs

---
+++ src/graphql/routes.rs
@@ -0,0 +1,18 @@
+use actix_web::{HttpResponse, web, get, post};
+use async_graphql_actix_web::{GraphQLRequest, GraphQLResponse};
+use async_graphql::http::PlaygroundConfig;
+use super::AppState;
+
+#[post("/graphql")]
+pub async fn graphql(state: web::Data<AppState>, request: GraphQLRequest) -> GraphQLResponse {
+    state.schema.execute(request.into_inner()).await.into()
+}
+
+#[get("/graphql")]
+pub async fn graphiql() -> HttpResponse {
+    HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(async_graphql::http::playground_source(
+            PlaygroundConfig::new("/graphql"),
+        ))
+}
