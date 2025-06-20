045d6aacaec678f9cb99fe870d4eb491192a2618
Roy Murphy <roycoins@gmail.com>
2025-06-20 17:55:19 +0100

    Create route.rs

---
+++ src/websocket/route.rs
@@ -0,0 +1,17 @@
+use actix_web::{web, HttpResponse};
+use actix_web_actors::ws;
+use super::super::AppState;
+use super::Subscriber;
+
+pub async fn ws_route(
+    req: actix_web::HttpRequest,
+    stream: web::Payload,
+    state: web::Data<AppState>,
+) -> Result<HttpResponse, actix_web::Error> {
+    let subscriber = Subscriber {
+        id: uuid::Uuid::new_v4().to_string(),
+        tx: state.tx_channel.clone(),
+        state: state.get_ref().clone(),
+    };
+    ws::start(subscriber, &req, stream)
+}
