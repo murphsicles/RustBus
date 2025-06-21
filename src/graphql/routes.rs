9881ddeb64fa961197fe7986947bfc624054407f
Roy Murphy <roycoins@gmail.com>
2025-06-21 14:49:26 +0100

    Update routes.rs

--- src/graphql/routes.rs
+++ src/graphql/routes.rs
@@ -1,6 +1,6 @@
 use actix_web::{HttpResponse, web, get, post};
 use async_graphql_actix_web::{GraphQLRequest, GraphQLResponse};
-use async_graphql::http::playground_source;
+use async_graphql::http::{playground_source, GraphQLPlaygroundConfig};
 use super::AppState;
 
 #[post("/graphql")]
@@ -12,5 +12,5 @@ pub async fn graphql(state: web::Data<AppState>, request: GraphQLRequest) -> Gra
 pub async fn graphiql() -> HttpResponse {
     HttpResponse::Ok()
         .content_type("text/html; charset=utf-8")
-        .body(playground_source("/graphql"))
+        .body(playground_source(GraphQLPlaygroundConfig::new("/graphql")))
 }
