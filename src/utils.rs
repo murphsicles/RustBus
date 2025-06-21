acc5c976bf2afa9adc172c327add3df6d7ea2d23
Roy Murphy <roycoins@gmail.com>
2025-06-21 15:08:17 +0100

    Update utils.rs

--- src/utils.rs
+++ src/utils.rs
@@ -1,9 +1,10 @@
 use sv::messages::Tx;
+use sv::util::Serializable;
 
 pub fn extract_op_return(tx: &Tx) -> Option<String> {
     tx.outputs.iter()
-        .find(|out| out.script.is_op_return())
-        .and_then(|out| Some(hex::encode(&out.script.data)))
+        .find(|out| out.lock_script.is_op_return())
+        .and_then(|out| Some(hex::encode(&out.lock_script.data)))
 }
 
 pub trait TxExt {
