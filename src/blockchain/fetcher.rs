040d90742cf590d12ff5607f6a400397744ca866
Roy Murphy <roycoins@gmail.com>
2025-06-23 04:40:30 +0100

    Update fetcher.rs

--- src/blockchain/fetcher.rs
+++ src/blockchain/fetcher.rs
@@ -7,7 +7,7 @@ use log::info;
 use serde_json::{Value, to_value};
 use std::io::Cursor;
 
-fn into_json<T: serde::Serialize>(val: T) -> Result<Value, Box<dyn std::error::Error>> {
+fn into_json<T: serde::Serialize>(val: T) -> Result<Value, Box<dyn std::error::Error + Send + Sync>> {
     Ok(to_value(val)?)
 }
 
@@ -16,13 +16,13 @@ pub struct BlockFetcher {
 }
 
 impl BlockFetcher {
-    pub fn new(rpc_url: &str, rpc_user: &str, rpc_password: &str, _network: Network) -> Result<Self, Box<dyn std::error::Error>> {
+    pub fn new(rpc_url: &str, rpc_user: &str, rpc_password: &str, _network: Network) -> Result<Self, Box<dyn std::error::Error + Send + Sync>> {
         let rpc = RpcClient::new(rpc_url, bitcoinsv_rpc::Auth::UserPass(rpc_user.to_string(), rpc_password.to_string()), None)?;
         info!("Connected to BSV node RPC at {}", rpc_url);
         Ok(BlockFetcher { rpc })
     }
 
-    pub fn fetch_block(&mut self, block_hash: &str) -> Result<(Block, i64), Box<dyn std::error::Error>> {
+    pub fn fetch_block(&mut self, block_hash: &str) -> Result<(Block, i64), Box<dyn std::error::Error + Send + Sync>> {
         let block_bytes = hex::decode(block_hash)?;
         let block_hash = BlockHash::from(&block_bytes[..]);
         let block_hex: Value = self.rpc.call("getblock", &[into_json(block_hash)?, 0.into()])?;
@@ -35,8 +35,15 @@ impl BlockFetcher {
         Ok((block, height))
     }
 
-    pub fn get_block_hash(&mut self, height: i64) -> Result<String, Box<dyn std::error::Error>> {
+    pub fn get_block_hash(&mut self, height: i64) -> Result<String, Box<dyn std::error::Error + Send + Sync>> {
         let hash = self.rpc.get_block_hash(height as u64)?;
         Ok(hash.to_string())
+    }
+
+    pub fn get_best_block_height(&mut self) -> Result<i64, Box<dyn std::error::Error + Send + Sync>> {
+        let hash = self.rpc.get_best_block_hash()?;
+        let block: Value = self.rpc.call("getblock", &[into_json(hash)?, 1.into()])?;
+        let height = block["height"].as_i64().ok_or_else(|| "Missing height")?;
+        Ok(height)
     }
 }
