8fc29975e1eae05f267fa6441b72437cf00a63bf
Roy Murphy <roycoins@gmail.com>
2025-06-24 20:40:25 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -74,7 +74,7 @@ async fn main() -> std::io::Result<()> {
                         let schema = state.schema.clone();
                         GraphQLResponse::from(schema.execute(req.into_inner()).await)
                     }))
-                    .route(web::get().to(|_req: HttpRequest| async { graphiql() })),
+                    .route(web::get().to(|_req: HttpRequest| async { graphiql })),
             )
     })
     .bind(&config.bind_addr)?
