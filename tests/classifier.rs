5038a95c89a20965b0d27f8dfd416b72615a3cd0
Roy Murphy <roycoins@gmail.com>
2025-06-25 03:24:46 +0100

    Update classifier.rs

--- tests/classifier.rs
+++ tests/classifier.rs
@@ -10,7 +10,10 @@ mod tests {
     fn test_classify_run() {
         let classifier = TransactionClassifier::new();
         let mut tx = Tx::default();
-        let script = Script::new_op_return(b"run://test".to_vec());
+        let data = b"run://test";
+        let mut script_bytes = vec![0x6a, data.len() as u8]; // OP_RETURN, length
+        script_bytes.extend_from_slice(data); // Append data
+        let script = Script(script_bytes);
         tx.outputs.push(TxOut { satoshis: 0, lock_script: script });
         assert_eq!(classifier.classify(&tx), "RUN");
     }
