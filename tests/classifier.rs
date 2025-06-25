efdddf1079523c78543037afa81d80ac706fa60c
Roy Murphy <roycoins@gmail.com>
2025-06-25 03:18:26 +0100

    Update classifier.rs

--- tests/classifier.rs
+++ tests/classifier.rs
@@ -4,13 +4,13 @@ mod tests {
     use sv::messages::TxOut;
     use sv::script::Script;
     #[cfg(test)]
-    use crate::blockchain::classifier::TransactionClassifier;
+    use rustbus::blockchain::classifier::TransactionClassifier;
 
     #[test]
     fn test_classify_run() {
         let classifier = TransactionClassifier::new();
         let mut tx = Tx::default();
-        let script = Script::op_return(b"run://test");
+        let script = Script::new_op_return(b"run://test".to_vec());
         tx.outputs.push(TxOut { satoshis: 0, lock_script: script });
         assert_eq!(classifier.classify(&tx), "RUN");
     }
